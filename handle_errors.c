90e9d735864dda151354a3b372fbd8341b100daa
Anashm24 <anashoumaid19@gmail.com>
2023-12-14 19:08:46 +0100

    update

--- handle_errors.c
+++ handle_errors.c
@@ -52,7 +52,6 @@ void handle_error(int error_number, ...)
  * (6) => When the stack it empty for pint.
  * (7) => When the stack it empty for pop.
  * (8) => When stack is too short for operation.
- * (9) => Division by zero.
  */
 void more_error(int error_number, ...)
 {
@@ -75,38 +74,6 @@ void more_error(int error_number, ...)
 			line_number = va_arg(args, unsigned int);
 			opcode = va_arg(args, char *);
 			fprintf(stderr, "L%d: can't %s, stack too short\n", line_number, opcode);
-			break;
-		case 9:
-			fprintf(stderr, "L%d: division by zero\n",
-				va_arg(args, unsigned int));
-			break;
-		default:
-			break;
-	}
-	free_nodes();
-	exit(EXIT_FAILURE);
-}
-
-/**
- * str_error - handles errors.
- * @error_number: The error codes are the following:
- * (10) ~> The number inside a node is outside ASCII bounds.
- * (11) ~> The stack is empty.
- */
-void str_error(int error_number, ...)
-{
-	va_list args;
-	int line_number;
-
-	va_start(args, error_number);
-	line_number = va_arg(args, int);
-	switch (error_number)
-	{
-		case 10:
-			fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
-			break;
-		case 11:
-			fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 			break;
 		default:
 			break;
